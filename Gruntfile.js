26a4a270b0cf0d75ebdf5392f85c8e98ee1cbb95
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-09-05 11:01:02 +0200

    Add and set up grunt-contrib-sass

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+  
+    grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
+      sass: {
+        dist: {
+          options: {
+            style: 'compressed'
+          },
+          files: {
+            'style.min.css':'sass/style.scss',
+          }
+        }
+      }
+    });
+  
+    grunt.loadNpmTasks('grunt-contrib-sass');
+  
+    grunt.registerTask('default', ['sass']);
+  };
